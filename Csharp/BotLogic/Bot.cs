756f7f9ebfd216b2fe7aa2773844f47d496b35a2
Fabrice Lajoie <lajoie.fabrice1@gmail.com>
2026-07-25 14:05:25 -0400

    trying

--- Csharp/BotLogic/Bot.cs
+++ Csharp/BotLogic/Bot.cs
@@ -13,43 +13,44 @@ public class Bot : IBot
 
     public ActionBase? GetNextAction(GameState state)
     {
+        return null;
         // foreach (var resources in state.VisibleResources)
         // {
         //     Console.WriteLine($"{resources.Position}");
         // }
-        PlayerInfo bot = state.Bot;
-        Resource target = state.VisibleResources
-            .Where(r => r.CurrentAmount > 0)
-            .OrderBy(r => Moving.ManhattanDist(bot.Position, r.Position))
-            .FirstOrDefault();
+        // PlayerInfo bot = state.Bot;
+        // Resource target = state.VisibleResources
+        //     .Where(r => r.CurrentAmount > 0)
+        //     .OrderBy(r => Moving.ManhattanDist(bot.Position, r.Position))
+        //     .FirstOrDefault();
 
-        // return new DestroyStructureAction(new Position(141, 324));
-        // return new PlaceExtractorAction(new Position(184, 288));
-        // return new MoveAction(new Position(state.Bot.Position.X, state.Bot.Position.Y + 1));
-        // Position new_p = Moving.GoTo(bot.Position, new Position(141, 324), state.VisibleTiles);
-        // return new MoveAction(new_p);
+        // // return new DestroyStructureAction(new Position(141, 324));
+        // // return new PlaceExtractorAction(new Position(184, 288));
+        // // return new MoveAction(new Position(state.Bot.Position.X, state.Bot.Position.Y + 1));
+        // // Position new_p = Moving.GoTo(bot.Position, new Position(141, 324), state.VisibleTiles);
+        // // return new MoveAction(new_p);
 
-        // foreach (KeyValuePair<(int, int), Tile> pair in state.VisibleTiles)
+        // // foreach (KeyValuePair<(int, int), Tile> pair in state.VisibleTiles)
+        // // {
+        // //     Tile tile = pair.Value;
+        // //     if (tile.TerrainCategory == "Liquid")
+        // //         Console.WriteLine($"{tile.TerrainCategory}");
+        // // }
+
+        // if (Moving.ManhattanDist(bot.Position, target.Position) > 1)
         // {
-        //     Tile tile = pair.Value;
-        //     if (tile.TerrainCategory == "Liquid")
-        //         Console.WriteLine($"{tile.TerrainCategory}");
+        //     Position new_p = Moving.GoTo(bot.Position, new Position(184, 288), state.VisibleTiles);
+        //     return new MoveAction(new_p);
         // }
-
-        if (Moving.ManhattanDist(bot.Poassition, target.Position) > 1)
-        {
-            Position new_p = Moving.GoTo(bot.Position, new Position(184, 288), state.VisibleTiles);
-            return new MoveAction(new_p);
-        }
-        else if (bot.Inventory.Any(item => item.Quantity >= 30))
-        {
-            Console.WriteLine($"Companion");
-            return new SendCompanionAction();
-        }
-        else
-        {
-            Console.WriteLine($"Gathering");
-            return new GatherNodeAction(target.Position);
-        }
+        // else if (bot.Inventory.Any(item => item.Quantity >= 30))
+        // {
+        //     Console.WriteLine($"Companion");
+        //     return new SendCompanionAction();
+        // }
+        // else
+        // {
+        //     Console.WriteLine($"Gathering");
+        //     return new GatherNodeAction(target.Position);
+        // }
     }
 }
